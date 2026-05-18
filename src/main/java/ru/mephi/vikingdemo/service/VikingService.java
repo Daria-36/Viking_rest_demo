61de537b4557de8e1b260e578f16c95e512298c5
Daria-36 <darushckap@gmail.com>
2026-05-18 03:49:33 +0300

    Update VikingService.java

--- src/main/java/ru/mephi/vikingdemo/service/VikingService.java
+++ src/main/java/ru/mephi/vikingdemo/service/VikingService.java
@@ -4,18 +4,15 @@ import org.springframework.stereotype.Service;
 import ru.mephi.vikingdemo.model.Viking;
 
 import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import ru.mephi.vikingdemo.repository.VikingStorage;
 
 @Service
 public class VikingService {
-    // каждый раз при изменении создаётся новая копия списка 
 
     private final VikingFactory vikingFactory;
     private final VikingStorage vikingStorage;
-    
-    
+
     @Autowired
     public VikingService(
             VikingFactory vikingFactory,
@@ -24,7 +21,7 @@ public class VikingService {
         this.vikingFactory = vikingFactory;
         this.vikingStorage = vikingStorage;
     }
-    
+
     public List<Viking> findAll() {
         return vikingStorage.findAll();
     }
@@ -33,6 +30,15 @@ public class VikingService {
         Viking viking = vikingFactory.createRandomViking();
         return vikingStorage.save(viking);
     }
+
+    public Viking addViking(Viking viking) {
+        return vikingStorage.save(viking);
+    }
+
+    public Viking updateById(int id, Viking viking) {
+        return vikingStorage.updateById(id, viking);
+    }
+
     public void deleteById(int id) {
         vikingStorage.deleteById(id);
     }
